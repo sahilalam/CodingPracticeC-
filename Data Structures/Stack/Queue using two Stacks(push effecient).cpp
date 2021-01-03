5fd36231b88cefb3e18ee6162b6266a1341bb558
sahilalam <alamsahil939@gmail.com>
2021-01-03 16:22:12 +0530

    stacks updated

--- Data Structures/Stack/Queue using two Stacks(push effecient).cpp
+++ Data Structures/Stack/Queue using two Stacks(push effecient).cpp
@@ -1,3 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+
+};
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+
+
+}
 /* The structure of the class is
 class StackQueue{
 private:
